a82e67cdff8b6577b9d98b038177df005b9c2de3
prmiguel <miangel-pr@outlook.com>
2024-06-20 16:36:57 +0000

    Update current tests

--- src/test/java/api/test/restassured/GetPublicCrocodileByIdTests.java
+++ src/test/java/api/test/restassured/GetPublicCrocodileByIdTests.java
@@ -1,14 +1,30 @@
 package api.test.restassured;
 
 import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class GetPublicCrocodileByIdTests {
+
     @Test
     public void shouldListAPublicCrocodile() {
-        when()
-                .get("https://test-api.k6.io/public/crocodiles/1")
-                .then()
-                .statusCode(200);
+        RestAssured.baseURI = "https://test-api.k6.io";
+        
+        given().
+            pathParams("id", 1).
+        when().
+            get("/public/crocodiles/{id}").
+        then().
+            assertThat().
+            statusCode(200).
+            header("Content-Type", "application/json").
+            body("id", equalTo(1)).
+            body("name", equalTo("Bert")).
+            body("sex", equalTo("M")).
+            body("date_of_birth", equalTo("2010-06-27")).
+            body("age", equalTo(13));
     }
 }
--- src/test/java/api/test/restassured/ListPublicCrocodilesTests.java
+++ src/test/java/api/test/restassured/ListPublicCrocodilesTests.java
@@ -1,14 +1,24 @@
 package api.test.restassured;
 
+import static org.hamcrest.Matchers.equalTo;
+
 import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
 import static io.restassured.RestAssured.*;
 
 public class ListPublicCrocodilesTests {
     @Test
     public void shouldListAllPublicCrocodiles() {
-        when()
-                .get("https://test-api.k6.io/public/crocodiles")
-                .then()
-                .statusCode(200);
+        RestAssured.baseURI = "https://test-api.k6.io";
+        
+        given().
+        when().
+            get("/public/crocodiles").
+        then().
+            assertThat().
+            statusCode(200).
+            body("size()", equalTo(8));
     }
 }
